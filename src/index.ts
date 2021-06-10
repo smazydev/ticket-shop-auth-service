89369806f3bbf4f7a9a3e9823e7d5539ea1e0164
smazydev <alizafar1437@gmail.com>
2021-06-10 04:42:30 -0400

    added database connection to auth service

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import "express-async-errors";
-import {json} from "body-parser";
-import {currentUserRouter} from "./routes/current-user";
-import {signInRouter} from "./routes/signin";
-import {signOutRouter} from "./routes/signout";
-import {signUpRouter} from "./routes/signup";
-import {errorHandler} from "./middlewares/error-handler";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
@@ -16,13 +18,27 @@ app.use(signUpRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 
-app.get('*',() => {
-    throw new NotFoundError();
-})
+app.get("*", () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
+const startConnection = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-service:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("connected")
+  } catch (err) {
+    console.log(err);
+  }
 
-app.listen(3000,() => {
+  app.listen(3000, () => {
     console.log("listening on port 3000!!!!!!!!!!");
-})
+  });
+};
+
+startConnection();
