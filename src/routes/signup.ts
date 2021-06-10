eb09b302c5ed831b45a5fe1f7a6a6e1a72e755a0
smazydev <alizafar1437@gmail.com>
2021-06-10 06:24:29 -0400

    added create user functionality and added comments

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,33 +1,43 @@
-import express, {Request,Response} from "express";
+import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import {RequestValidationError} from "../errors/request-validation-error";
-import {DatabaseConnectionError} from "../errors/database-connection-error";
+import { User } from "../models/users";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
-
 router.post(
   "/api/users/signup",
   [
+    //Email and Password Validation Middleware
     body("email").isEmail().withMessage("Email must be valid"),
     body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req:Request, res:Response) => {
+  async (req: Request, res: Response) => {
+    
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+      throw new RequestValidationError(errors.array());
     }
-
+    
+    //Extract email and password from the body of the request
     const { email, password } = req.body;
-    console.log("creating user")
+    const existingUser = await User.findOne({ email });
+    
+    //Check if user email already exists
+    if (existingUser) {
+      console.log("Email in use");
+      return res.send({});
+    }
+    
+    //Create a new user and save it to the database
+    const user = User.build({ email, password });
+    await user.save();
 
-    throw new DatabaseConnectionError();
-
-    res.send({});
-
+    //201 Created Response and send the user object back to the user.
+    res.status(201).send(user);
   }
 );
 
