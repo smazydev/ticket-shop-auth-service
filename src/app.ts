611a61bab404c37fc6d2340ada90e043705f285d
smazydev <alizafar1437@gmail.com>
2021-06-11 04:04:33 -0400

    setup test environment

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+//Route Imports
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}))
+
+
+//Initialize Routes
+app.use(currentUserRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+
+app.get("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app}
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
+import {app} from "./app";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-//Route Imports
-import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/signin";
-import { signOutRouter } from "./routes/signout";
-import { signUpRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}))
-
-
-//Initialize Routes
-app.use(currentUserRouter);
-app.use(signUpRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-
-app.get("*", () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
-
 
 const startConnection = async () => {
 
