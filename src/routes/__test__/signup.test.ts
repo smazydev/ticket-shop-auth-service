86d42d47e62f7fd78225ad2a8f01c471263af40d
smazydev <alizafar1437@gmail.com>
2021-06-11 05:53:29 -0400

    added tests for signup functionality

--- src/routes/__test__/signup.test.ts
+++ src/routes/__test__/signup.test.ts
@@ -1,9 +1,57 @@
 import request from "supertest";
-import {app} from "../../app";
+import { app } from "../../app";
 
 it("returns a 201 on sucessful signup", async () => {
-	return request(app).post("api/users/signup").send({
-		email:"test@test.com",
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  return await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "asdfdasd",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "p",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  return await request(app).post("/api/users/signup").send({}).expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+	await request(app).post("/api/users/signup").send({
+		email: 'test@test.com',
+		password: 'password'
+	}).expect(201)
+
+	await request(app).post("/api/users/signup").send({
+		email: "test@test.com",
 		password: "password"
+	}).expect(400)
+})
+
+it("sets a cookie after successful signup", async () => {
+	const response = await request(app).post("/api/users/signup").send({
+		email: 'test@test.com',
+		password: 'password'
 	}).expect(201);
+
+	expect(response.get("Set-Cookie")).toBeDefined();
 })
